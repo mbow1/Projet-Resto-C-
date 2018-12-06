9e4717b71bf6551ecbe3df8e23bfa231dbd9d04a
mbow1 <mbow.morniass@gmail.com>
2018-12-06 14:35:21 +0100

    form

--- simulationRestaurant/Vue/Form1.Designer.cs
+++ simulationRestaurant/Vue/Form1.Designer.cs
@@ -29,12 +29,185 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Pause = new System.Windows.Forms.Button();
+            this.button_quitter = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column_Client = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.dataGridView6 = new System.Windows.Forms.DataGridView();
+            this.dataGridView7 = new System.Windows.Forms.DataGridView();
+            this.checkBox_modeSimul = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(781, 40);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(75, 23);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            // 
+            // button_Pause
+            // 
+            this.button_Pause.Location = new System.Drawing.Point(1023, 40);
+            this.button_Pause.Name = "button_Pause";
+            this.button_Pause.Size = new System.Drawing.Size(75, 23);
+            this.button_Pause.TabIndex = 1;
+            this.button_Pause.Text = "Pause";
+            this.button_Pause.UseVisualStyleBackColor = true;
+            // 
+            // button_quitter
+            // 
+            this.button_quitter.Location = new System.Drawing.Point(1127, 40);
+            this.button_quitter.Name = "button_quitter";
+            this.button_quitter.Size = new System.Drawing.Size(75, 23);
+            this.button_quitter.TabIndex = 2;
+            this.button_quitter.Text = "Quitter";
+            this.button_quitter.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column_Client});
+            this.dataGridView1.Location = new System.Drawing.Point(781, 103);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(113, 45);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Column_Client
+            // 
+            this.Column_Client.HeaderText = "Client";
+            this.Column_Client.Name = "Column_Client";
+            this.Column_Client.Width = 70;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(781, 365);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(113, 231);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(781, 187);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(113, 150);
+            this.dataGridView3.TabIndex = 6;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(914, 446);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(274, 150);
+            this.dataGridView4.TabIndex = 7;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(914, 279);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(274, 150);
+            this.dataGridView5.TabIndex = 8;
+            // 
+            // dataGridView6
+            // 
+            this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView6.Location = new System.Drawing.Point(1062, 103);
+            this.dataGridView6.Name = "dataGridView6";
+            this.dataGridView6.Size = new System.Drawing.Size(121, 150);
+            this.dataGridView6.TabIndex = 9;
+            // 
+            // dataGridView7
+            // 
+            this.dataGridView7.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView7.Location = new System.Drawing.Point(914, 103);
+            this.dataGridView7.Name = "dataGridView7";
+            this.dataGridView7.Size = new System.Drawing.Size(126, 150);
+            this.dataGridView7.TabIndex = 10;
+            // 
+            // checkBox_modeSimul
+            // 
+            this.checkBox_modeSimul.AutoSize = true;
+            this.checkBox_modeSimul.Location = new System.Drawing.Point(890, 43);
+            this.checkBox_modeSimul.Name = "checkBox_modeSimul";
+            this.checkBox_modeSimul.Size = new System.Drawing.Size(102, 17);
+            this.checkBox_modeSimul.TabIndex = 11;
+            this.checkBox_modeSimul.Text = "Mode simulation";
+            this.checkBox_modeSimul.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(750, 556);
+            this.panel1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1214, 641);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox_modeSimul);
+            this.Controls.Add(this.dataGridView7);
+            this.Controls.Add(this.dataGridView6);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_quitter);
+            this.Controls.Add(this.button_Pause);
+            this.Controls.Add(this.button_Start);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Pause;
+        private System.Windows.Forms.Button button_quitter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.DataGridView dataGridView6;
+        private System.Windows.Forms.DataGridView dataGridView7;
+        private System.Windows.Forms.CheckBox checkBox_modeSimul;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_Client;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
