e1097f61c29d17d4535aa20b9d0bce77bd67e9d3
kayliechloe <chlochlocc@gmail.com>
2018-12-07 16:14:53 +0100

    classes

---
+++ simulationRestaurant/Model/Acteur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public abstract class Acteur
+    {
+        int id;
+        float speed;
+
+        Position pos = new Position(0,0);
+        /*Sprite sprit = new Sprite();*/
+
+    }
+}
